3b8356a9a3dfc2292dffc7a37e328225a470c73b
carissa morrow <carissamorrow@hotmail.com>
2018-11-01 11:12:02 -0600

    finished

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let snibley = {
   moodIndex: 0,
   moods: ["happy", "grumpy", "bitey"],
   tolerance: 3,
-  images: ["https://i.ytimg.com/vi/hrxR6sFiq04/maxresdefault.jpg", "https://i.ytimg.com/vi/yVN-0k3zK8s/maxresdefault.jpg"]
+  images: ["https://i.ytimg.com/vi/hrxR6sFiq04/maxresdefault.jpg", "https://static.boredpanda.com/blog/wp-content/uploads/2017/01/fb-image-sharing-dashboard_587514478bd22.jpg"]
 }
 
 function pet() {
@@ -24,22 +24,12 @@ function reset() {
 
 function draw() {
   let template = ''
-  if (snibley.pets < snibley.tolerance) {
-    template = `
+  template = `
   <img src="${snibley.images[snibley.moodIndex]}"/>
         <h2>Name: ${snibley.name}</h2>
         <h2>Pets: ${snibley.pets}</h2>
         <h2>Mood: ${snibley.moods[snibley.moodIndex]}</h2>
       `
-  }
-  else {
-    template = `
-      < img src = "${snibley.images[snibley.moodIndex]}"/>
-        <h2>Name: ${snibley.name}</h2>
-        <h2>Pets: ${snibley.pets}</h2>
-        <h2>Mood: ${snibley.moods[snibley.moodIndex]}</h2>
-        `
-  }
   document.getElementById('cat-stats').innerHTML = template
 
 }
